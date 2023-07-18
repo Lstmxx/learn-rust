9b234bdc3dc2e136377be3713b8a08adef5078d4
lstmxx <740719284@qq.com>
2023-07-18 23:01:15 +0800

    feat: gues

--- basic/guessing_game/src/main.rs
+++ basic/guessing_game/src/main.rs
@@ -1,4 +1,7 @@
 use std::io;
+use rand::Rng;
+
+use std::cmp::Ordering;
 
 fn main() {
     println!("猜数");
@@ -8,5 +11,16 @@ fn main() {
 
     println!("你猜的是：{}", guess);
     
-    // let secret_number = rand::
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    println!("神秘数字是！！！： {}", secret_number);
+
+    let guess:u32 = guess.trim().parse().expect("转换失败捏");
+
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("small !"),
+        Ordering::Greater => println!("big !"),
+        Ordering::Equal => println!("yes!"),
+    }
 }
